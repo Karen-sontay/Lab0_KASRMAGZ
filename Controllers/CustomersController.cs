7f76bc02436155cf297f8a57d41f128ff013d545
KAREN ALEJANDRA SONTAY REYES <kasontay@correo.url.edu.gt>
2021-02-03 19:03:33 -0600

    Hola!!!

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Lab0_KASRMAGZ.Models.Data;
+
+
+namespace Lab0_KASRMAGZ.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: CustomersController
+        public ActionResult Index()
+        {
+            return View(Singleton.Instance.CustomersList);
+        }
+
+        // GET: CustomersController/Details/5
+        public ActionResult Details(int id)
+        {
+            var ViewCustomers = Singleton.Instance.CustomersList.Find(x => x.Id == id);
+            return View(ViewCustomers);
+        }
+
+        // GET: CustomersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                var newCustomers = new Models.Customers
+                {
+                    Id = Convert.ToInt32(collection["Id"]),
+                    Name = collection["Name"],
+                    LastName = collection["LastName"],
+                    Telephone = Convert.ToInt32(collection["Telephone"]),
+                    Description = collection["Description"]
+                };
+                Singleton.Instance.CustomersList.Add(newCustomers);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var EditCustomers = Singleton.Instance.CustomersList.Find(x => x.Id == id);
+            return View(EditCustomers);
+        }
+
+        // POST: CustomersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                var DeleteCustomers = Singleton.Instance.CustomersList.Find(x => x.Id == id);
+                Singleton.Instance.CustomersList.Remove(DeleteCustomers);
+
+                var UpdateCustomers = new Models.Customers
+                {
+                    Id = Convert.ToInt32(collection["Id"]),
+                    Name = collection["Name"],
+                    LastName = collection["LastName"],
+                    Telephone = Convert.ToInt32(collection["Telephone"]),
+                    Description = collection["Description"]
+                };
+
+                Singleton.Instance.CustomersList.Add(UpdateCustomers);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var DeleteCustomers = Singleton.Instance.CustomersList.Find(x => x.Id == id);
+                Singleton.Instance.CustomersList.Remove(DeleteCustomers);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
