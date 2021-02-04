f9d1183556ba9ed24cd4d5da03797cbfc7d75ccc
Argentina Garcia <margent.garz1926@gmail.com>
2021-02-03 18:47:07 -0600

    Agregar texto

---
+++ Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0_KASRMAGZ.Models
+{
+    public class Customers
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Telephone { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Data/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0_KASRMAGZ.Models.Data
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+        public List<Customers> CustomersList;
+        private Singleton()
+        {
+            CustomersList = new List<Customers>();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
